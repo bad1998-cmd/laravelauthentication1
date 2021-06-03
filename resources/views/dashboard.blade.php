0a88c48425bf23d3d1966df4f880af56215857b8
deepuinteriordesign <deepuinteriordesign@gmail.com>
2021-06-03 13:46:34 +0530

    First stage

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,11 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h3> Welcome to </h3>
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+        <h1> Page</h1>
+    </x-slot>
+
+    
+</x-app-layout>
